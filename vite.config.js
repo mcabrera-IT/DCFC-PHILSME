68290dcf152d0dcedac7368f514adfa2b93c3f4a
Michelangelo Cabrera <cabrera.michelangelo0502@gmail.com>
2026-04-27 16:19:13 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['philsme.discoverycapital.com.ph'],
+    allowedHosts: true,
   },
 })
